d615d6513b777bb4f2801793a813f40b2c4686e5
Decode777 <93553240+Decode777@users.noreply.github.com>
2024-10-07 23:27:41 +0530

    Time: 2 ms (16.1%), Space: 42.1 MB (11.21%) - LeetHub

---
+++ 0075-sort-colors/0075-sort-colors.java
@@ -0,0 +1,16 @@
+class Solution {
+    public void sortColors(int[] nums) {
+        //Bubble sort
+        int n = nums.length;
+        for(int i=0;i<n;i++){
+            for(int j=1;j<n;j++){
+                if(nums[j-1]>nums[j]){
+                    nums[j]^=nums[j-1];
+                    nums[j-1]^= nums[j];
+                    nums[j]^=nums[j-1];
+
+                }
+            }
+        }
+    }
+}
