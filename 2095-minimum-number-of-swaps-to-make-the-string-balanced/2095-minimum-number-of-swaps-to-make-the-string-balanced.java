c82df4ac478bed809eb5334a6ac22c394f3ffa10
Decode777 <93553240+Decode777@users.noreply.github.com>
2024-10-08 16:22:56 +0530

    Time: 13 ms (93.08%), Space: 55.9 MB (14.49%) - LeetHub

---
+++ 2095-minimum-number-of-swaps-to-make-the-string-balanced/2095-minimum-number-of-swaps-to-make-the-string-balanced.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int minSwaps(String s) {
+        int balance = 0, swaps = 0;
+        for (char ch : s.toCharArray()) {
+            if (ch == '[') {
+                balance++;
+            } else {
+                balance--;
+            }
+            if (balance < 0) {
+                swaps++;
+                balance = 1;
+            }
+        }
+        return swaps;
+    }
+}
